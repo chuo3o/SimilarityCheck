67cd0227013469e8df3a0b7b9cd522c4002c32c5
yenna <yena.gwak@gmail.com>
2025-07-23 15:20:30 +0900

    [feature] add SameLengthCheck TC and lengthCompare method

--- TDD2_SimilarityCheck/similaritycheck.cpp
+++ TDD2_SimilarityCheck/similaritycheck.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
 
-class Similarity {
+using namespace std;
+
+class SimilarityChecker {
 public:
+	bool lengthCompare(const string A, const string B) {
+		return (A.length() == B.length());
+	}
 
+	string stA{};
+	string stB{};
 };
--- TDD2_SimilarityCheck/similaritycheck_test.cpp
+++ TDD2_SimilarityCheck/similaritycheck_test.cpp
@@ -1,8 +1,15 @@
+#include <stdexcept>
 #include "gmock/gmock.h"
 #include "similaritycheck.cpp"
 
 using namespace testing;
 
+
 TEST(SimilarCheck, init) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(SimilarCheck, SameLengthCheck) {
+	SimilarityChecker sim;
+	EXPECT_TRUE(sim.lengthCompare("aaa", "bbb"));
 }
