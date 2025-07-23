883a445fdb415f2bfd96616c8baa34b752303aa9
yenna <yena.gwak@gmail.com>
2025-07-23 16:57:59 +0900

    [feature] add const value

--- TDD2_SimilarityCheck/similaritycheck.cpp
+++ TDD2_SimilarityCheck/similaritycheck.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 class SimilarityChecker {
 public:
+	const int MAX_POINT_FROM_LENGTH = 60;
+	const int MAX_POINT_FROM_CHAR = 100 - MAX_POINT_FROM_LENGTH;
 	
 	int lengthCompare(const string A, const string B) {
 		alen = A.length(); 
@@ -12,9 +14,11 @@ public:
 		assertIllegalArgument();
 
 		if (blen > alen) {
-			float temp = alen; alen = blen; blen = temp;
+			float temp = alen; 
+			alen = blen; 
+			blen = temp;
 		}
-		return (alen >= blen * 2) ? 0 : int((1 - ((alen - blen) / blen)) * 60);
+		return (alen >= blen * 2) ? 0 : int((1 - ((alen - blen) / blen)) * MAX_POINT_FROM_LENGTH);
 	}
 
 	void assertIllegalArgument() {
